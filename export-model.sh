cf679053cfbd58b9c397953a075f339da9fcfd59
Carlos Badenes <cbadenes@gmail.com>
2018-05-18 13:37:44 +0200

    export a topic model

--- export-model.sh
+++ export-model.sh
@@ -1,4 +1,4 @@
-curl --header "Content-Type: application/json" \
+curl -u username:password --header "Content-Type: application/json" \
   --request POST \
   --data '{ "contactEmail": "string", "contactName": "string", "contactUrl": "string",  "credentials": { "email": "string",  "password": "string", "repository": "string", "username": "string"  },  "description": "string",  "licenseName": "Apache License Version 2.0",  "licenseUrl": "https://www.apache.org/licenses/LICENSE-2.0",  "title": "string"}' \
   http://localhost:7777/topics/exports
